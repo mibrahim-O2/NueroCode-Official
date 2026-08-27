99b0b37cdb6c2dee93335f6db4effeca3330209b
Muhammad Ibrahim <mibrahimkhalid306@gmail.com>
2026-08-28 00:34:25 +0500

    UI: add review-due lookup to roadmap service

--- frontend/src/services/roadmapService.js
+++ frontend/src/services/roadmapService.js
@@ -1,6 +1,7 @@
 import { apiClient } from './apiClient';
 
 export const getRoadmap = () => apiClient.get('/roadmap/');
+export const getReviewDue = () => apiClient.get('/roadmap/review-due');
 export const startNode = (nodeId) => apiClient.post(`/roadmap/${nodeId}/start`, {});
 export const completeNode = (nodeId) => apiClient.post(`/roadmap/${nodeId}/complete`, {});
 export const getLeaderboard = () => apiClient.get('/leaderboard/');
